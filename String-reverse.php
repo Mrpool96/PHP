56fb8c876172f2e4956b63ab1d6e2880e359ca8e
Mrpool <sagarkarambelkar@gmail.com>
2020-10-23 10:24:42 +0530

    String reverse Program

---
+++ String-reverse.php
@@ -0,0 +1,14 @@
+<html>
+<title>
+String- reverse
+</title>    
+    <body>
+        <h1>String Reverse Program</h1>
+        <?php
+
+        echo strrev("Reverse String Program");
+        //strrev() is used to reverse a string//
+
+        ?>
+    </body>
+</html>
