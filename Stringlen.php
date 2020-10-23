ddea7df55dec84c3743572da4d998139e6bacc01
Mrpool <sagarkarambelkar@gmail.com>
2020-10-23 10:30:12 +0530

    Calculate the string length using strlrn()

---
+++ Stringlen.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<title>String Lenght</title>
+<body>
+    <h1>String Lenght</h1>
+    <?
+        echo strlen("Calculate String Lenght")
+        //strlen() use to calculate the string lenght
+    ?>
+</body>
+</html>
