efbe4b893c0c3d4234ce44e99b76c541e3541dae
Mrpool <sagarkarambelkar@gmail.com>
2020-10-27 12:49:01 +0530

    JSON Encode and Decode

---
+++ JSON.php
@@ -0,0 +1,28 @@
+<html>
+    <div class="body">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>        
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="jumbotron jumbtron-fluid"><h1>JSON Encode() and Decode()</h1></div>
+                    <h2>
+                       <? echo "Today's date:-" .date("d/m/Y") . "<br>" ;
+
+                            date_default_timezone_set("Asia/Kolkata");
+                            echo "Time is:- " .date("H:i:sa") ."<br>" ;
+
+                            //json encode
+
+                            
+                       ?>
+                    </h2>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</html>
